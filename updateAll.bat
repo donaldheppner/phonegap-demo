b477db16a1ca2bb3226ca4486945a3d09a1cc9c8
Donald Heppner <donald.heppner@gmail.com>
2013-05-23 01:43:20 -0400

    Added cordova and jQuery Mobile demo.

--- updateAll.bat
+++ updateAll.bat
@@ -1,2 +1,4 @@
 robocopy www Windows\WindowsPhone8\www /purge /s /A+:R /XF cordova*.js config.xml
 robocopy www Android\assets\www /purge /s /A+:R /XF cordova*.js config.xml
+
+robocopy www ..\phonegap-build\www /s /A+:R /XF cordova*.js
