8b71754c1f44dbb628bd7e276455ec9a0a4c31ab
Donald Heppner <donald.heppner@gmail.com>
2013-05-22 22:05:23 -0400

    Creating basic project structure for Windows 8, Windows Phone 8 and Android PhoneGap projects

---
+++ Android/assets/www/cordova.js
@@ -0,0 +1,16 @@
+﻿(function () {
+
+    var VERSION = '2.7.0',
+        currentScript = 'cordova-' + VERSION + '.js',
+        scripts = document.getElementsByTagName('script');
+
+    for (var n = 0; n < scripts.length; n++) {
+        if (scripts[n].src.indexOf('cordova.js') > -1) {
+            var cordovaPath = scripts[n].src.replace('cordova.js', currentScript);
+            var scriptElem = document.createElement("script");
+            scriptElem.src = cordovaPath;
+            document.head.appendChild(scriptElem);
+        }
+    }
+
+})();
---
+++ updateAll.bat
@@ -0,0 +1,2 @@
+robocopy Common\www Windows\WindowsPhone8\www /purge /s /A+:R /XF cordova-*.js
+robocopy Common\www Android\assets\www /purge /s /A+:R /XF cordova-*.js
